6d517dc526a36772d69042fde5b971746c622222
Adriel <adriel.oliveira@bevicred.com.br>
2018-06-18 15:25:31 -0300

    subindo

---
+++ app/Http/Controllers/Api/ProductsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductsController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+}
